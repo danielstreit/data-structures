c6b0a45b9087a82a97270ba961b7287cb1e89cbc
Daniel Streit <Daniel.Streit@gmail.com>
2014-07-24 17:55:40 -0700

    Impelement functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    size && size--;
+    var result = storage[0];
+    for (var i = 0; i < size; i++) {
+      storage[i] = storage[i + 1];
+    }
+    delete storage[size];
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
