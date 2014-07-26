a2f0bf8dc69457ecf215542b57664097cc996fc0
Daniel Streit <Daniel.Streit@gmail.com>
2014-07-26 14:11:11 -0700

    Implement hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,23 +1,50 @@
 var HashTable = function(){
   this._limit = 8;
+  this._size = 0;
   this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  // If the bucket for this index has not been initialized, initialize it
+  if (this._storage.get(i) === undefined) {
+    this._storage.set(i, []);
+  }
+
+  // If the key already exists, override and return the value
+  this._storage.get(i).forEach(function(kvPair) {
+    if (kvPair[0] === k) {
+      kvPair[1] = v;
+      return v;
+    }
+  });
+
+  // If the key is not already in the hashtable, insert and return it
+  this._storage.get(i).push([k, v]);
+  this._size++;
+  return v;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      return bucket[j][1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
 
+  var bIndex;
+  this._storage.get(i).forEach(function(kvPair, index, bucket) {
+    if (kvPair[0] === k) {
+      bIndex = index;
+    }
+  });
+  this._storage.get(i).splice(bIndex, 1);
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
