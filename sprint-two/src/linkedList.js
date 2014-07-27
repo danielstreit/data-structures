0114f7be2e04beb6ad11a445c3d3b760c7d514bf
Daniel Streit <Daniel.Streit@gmail.com>
2014-07-26 18:25:03 -0700

    Implement double linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -9,13 +9,43 @@ var makeLinkedList = function(){
       list.tail = list.head;
     } else {
       list.tail.next = makeNode(value);
+      list.tail.next.prev = list.tail;
       list.tail = list.tail.next;
     }
   };
 
+  list.addToHead = function(value){
+    if (list.head === null) {
+      list.head = makeNode(value);
+      list.tail = list.head;
+    } else {
+      list.head.prev = makeNode(value);
+      list.head.prev.next = list.head;
+      list.head = list.head.prev;
+    }
+  };
+
+  list.removeTail = function() {
+    var oldTail = list.tail;
+    if (list.tail.prev) {
+      list.tail = list.tail.prev;
+      list.tail.next = null;
+    } else {
+      list.head = null;
+      list.tail = null;
+    }
+    return oldTail.value;
+  };
+
   list.removeHead = function(){
     var oldHead = list.head;
-    list.head = list.head.next;
+    if (list.head.next) { 
+      list.head = list.head.next;
+      list.head.prev = null; 
+    } else {
+      list.head = null;
+      list.tail = null;
+    }
     return oldHead.value;
   };
 
@@ -36,6 +66,7 @@ var makeNode = function(value){
   var node = {};
   node.value = value;
   node.next = null;
+  node.prev = null;
   return node;
 };
 
